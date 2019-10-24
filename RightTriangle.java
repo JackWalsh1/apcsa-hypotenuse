df90f7ff38f8ab47d3adf2b98738c3af32a06b37
Jack Walsh <jwalsh@ucvts.tec.nj.us>
2019-10-24 09:08:54 -0400

    Exercise complete.

---
+++ RightTriangle.java
@@ -0,0 +1,27 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class RightTriangle {
+
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        
+
+        System.out.print("\nEnter base: ");
+        double base = in.nextDouble();
+        System.out.print("Enter height: ");
+        double height = in.nextDouble();
+        System.out.print("Enter hypotenuse: ");
+        double hypotenuse = in.nextDouble();
+
+        if (Math.hypot(base, height) == hypotenuse) {
+            System.out.println("\nYes, that's a right triangle!");
+        } else {
+            System.out.println("\nNope...not a right triangle.");
+        }
+
+        in.close();
+    }
+
+}
